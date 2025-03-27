5593c4d369973e9458e8f51950e57937c6162d92
Aviv Neeman <135433593+aviv15616@users.noreply.github.com>
2025-03-27 14:55:13 +0200

    Update main.py

--- src/pcap_analyzer/main.py
+++ src/pcap_analyzer/main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from pcap_gui import PcapGUI
 
 if __name__ == "__main__":
